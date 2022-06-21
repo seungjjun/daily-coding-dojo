c4b58242d57107827ccbf312673b167dbc75e15b
seungjjun <il1uso@naver.com>
2022-06-21 09:10:54 +0900

    answer의 인덱스가 홀수면 "수" 짝수면 "박"을 넣어 입력받은 길이만큼 반복했다.

--- Level1/220621/app/src/main/java/Watermelon.java
+++ Level1/220621/app/src/main/java/Watermelon.java
@@ -2,6 +2,14 @@ public class Watermelon {
   public String solution(int n) {
     String answer = "";
 
+    for (int i = 0; i < n; i += 1) {
+      if (i % 2 == 0) {
+        answer += "수";
+      }
+      if (i % 2 == 1) {
+        answer += "박";
+      }
+    }
     return answer;
   }
 
--- Level1/220621/app/src/test/java/WatermelonTest.java
+++ Level1/220621/app/src/test/java/WatermelonTest.java
@@ -7,6 +7,7 @@ class WatermelonTest {
   void process() {
     Watermelon watermelon = new Watermelon();
 
-    
+    assertEquals("수박수", watermelon.solution(3));
+    assertEquals("수박수박", watermelon.solution(4));
   }
 }
