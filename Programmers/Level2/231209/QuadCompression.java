af7b664f2e7c419bf8246aa8974d10414b9ffe04
seungjjun <il1uso@naver.com>
2023-12-09 23:28:57 +0900

    쿼드압축 후 개수 세기

---
+++ Programmers/Level2/231209/QuadCompression.java
@@ -0,0 +1,40 @@
+public class QuadCompression {
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[] {count.zero, count.one};
+    }
+
+    private Count count(int offsetX, int offsetY, int size, int[][] arr) {
+        int h = size / 2;
+        for (int x = offsetX; x < offsetX + size; x += 1) {
+            for (int y = offsetY; y < offsetY + size; y += 1) {
+                if (arr[y][x] != arr[offsetY][offsetX]) {
+                    return count(offsetX, offsetY, h, arr)
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+                }
+            }
+        }
+
+        if (arr[offsetY][offsetX] == 1) {
+            return new Count(0, 1);
+        }
+
+        return new Count(1, 0);
+    }
+
+    private static class Count {
+        private final int zero;
+        private final int one;
+
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+
+        public Count add(Count count) {
+            return new Count(zero + count.zero, one + count.one);
+        }
+    }
+}
